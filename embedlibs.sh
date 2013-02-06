7f3a409b6407125829cfe6fab314e532b18c0445
wilkie <wilkie05@gmail.com>
2013-02-06 04:06:57 -0500

    Creates libc-base for D apps to link to C stdlib.

--- embedlibs.sh
+++ embedlibs.sh
@@ -3,9 +3,13 @@
 mkdir -p .tempobjs
 cd .tempobjs
 
+ar x ../../xomb/user/c/lib/cbindings.a
+cp ../local/x86_64-pc-xomb/lib/libc.a ../local/x86_64-pc-xomb/lib/libc-base.a
+ar d ../local/x86_64-pc-xomb/lib/libc-base.a crt0.o
+ar -rs ../local/x86_64-pc-xomb/lib/libc-base.a *.o
+
 ar x ../../xomb/runtimes/mindrt/drt0.a
 ar x ../../xomb/runtimes/mindrt/mindrt.a
-ar x ../../xomb/user/c/lib/cbindings.a
 ar -rs ../local/x86_64-pc-xomb/lib/libc.a *.o
 
 cd ..
